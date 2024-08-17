b801fd6d7903502122b78dac15b5f258db3796f6
Darren Tan <darren.tan0512@gmail.com>
2024-08-18 01:25:24 +0800

    Feat/context (#4)
    * feat: food item display and counter
    
    * feat:webpage footer
    
    * feat:app download

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import { useState } from 'react'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
 
@@ -12,6 +14,8 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   )
 }
